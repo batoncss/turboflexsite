4e67b6d4f626720f111d5ca54344cedbff707e30
batoncss <batoncss@yandex.ru>
2022-07-09 16:31:33 +0300

    add git ignore

--- turboflexsite/settings.py
+++ turboflexsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a^gz131&^n()^*l0)+luh5afnd#e1wg1z^=)#j_=v)a!_fmlbj'
+#SECRET_KEY = тут мог быть ваш ключик
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
