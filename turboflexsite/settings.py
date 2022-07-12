7b086b233f36095efc4c08ecdf0aee04cb3c8b64
batoncss <batoncss@yandex.ru>
2022-07-12 16:40:29 +0300

    update ALLOWED_HOSTS

--- turboflexsite/settings.py
+++ turboflexsite/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = config['keys']['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xn--90aipcokmbjn.xn--p1ai']
 
 
 # Application definition
