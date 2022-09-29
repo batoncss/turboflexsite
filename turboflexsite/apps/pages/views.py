b9789e6e99b8d13a20c902e309468963a80a0b29
Dmitriy <96142392+batoncss@users.noreply.github.com>
2022-09-29 17:53:45 +0300

    Delete turboflexsite directory

--- turboflexsite/apps/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def page_home(request):
-    return render(request, 'pages/home.html')
-
-
-def page_find(request):
-    return render(request, 'pages/find.html')
